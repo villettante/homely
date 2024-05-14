04d480b98692ee17009fcae7530f98dbeea5be0e
Diana Gliga <diana.c.gliga@gmail.com>
2024-05-14 13:46:26 +0300

    minor code refactoring

--- app/src/main/java/com/example/homely/MainActivity.java
+++ app/src/main/java/com/example/homely/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.homely;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -20,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homePageFragment).commit();
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setSelectedItemId(R.id.bottom_nav_home);
-        bottomNavigationView.setSelectedItemId(R.id.bottom_nav_devices);
-        bottomNavigationView.setSelectedItemId(R.id.bottom_nav_automation);
         bottomNavigationView.setSelectedItemId(R.id.bottom_nav_user);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_nav_automation);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_nav_devices);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_nav_home);
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.bottom_nav_home) {
